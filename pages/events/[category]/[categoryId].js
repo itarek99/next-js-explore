594759e011ef55418a191c277a42491e45e4b1d6
itarek99 <tarekulislam7060@gmail.com>
2023-01-13 00:50:51 +0600

    single event page added

--- pages/events/[category]/[categoryId].js
+++ pages/events/[category]/[categoryId].js
@@ -1,8 +1,43 @@
-import { useRouter } from 'next/router';
+import Image from 'next/image';
 
-const London = () => {
-  const router = useRouter();
-
-  return <h1>Funny Event In {eventId}</h1>;
+const London = ({ data }) => {
+  return (
+    <div>
+      <Image src={data.image} height='300' width='300' alt={data.title} />
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+    </div>
+  );
 };
 export default London;
+
+export const getStaticPaths = async () => {
+  const { allEvents } = await import('/data/data.json');
+
+  const allPaths = allEvents.map((event) => {
+    return {
+      params: {
+        category: event.city,
+        categoryId: event.id,
+      },
+    };
+  });
+
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const { allEvents } = await import('/data/data.json');
+  const id = context?.params?.categoryId;
+
+  const currentEvent = allEvents.find((event) => event.id === id);
+
+  return {
+    props: {
+      data: currentEvent,
+    },
+  };
+};
