6ff56bb52e4a5cf5b7ed42e25933bc3e8d3ea96a
itarek99 <tarekulislam7060@gmail.com>
2023-01-15 03:08:51 +0600

    about us page added

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,8 +1,8 @@
 const AboutUs = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>
+    <div className='about-us'>
+      <h1 className='about-us__title'>About Us</h1>
+      <p className='about-us__text'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptas similique, beatae dolorem aliquam
         veritatis molestias, doloribus sint nihil totam autem dicta temporibus excepturi quaerat maxime iusto magnam.
         Esse assumenda quos nostrum alias ab libero pariatur dicta adipisci! Voluptatibus incidunt eum sequi.
